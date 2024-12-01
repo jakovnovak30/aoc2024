c40cf71f2429d78c6d5520369424ab46061d06e6
jakovnovak30 <jakovnovak30@gmail.com>
2024-12-01 13:38:39 +0100

    day1 done

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -5,5 +5,27 @@ fn main() {
 
     let input = read_input("src/example.txt");
 
-    println!("{}", input);
+    let mut list1 : Vec<_> = input.clone().into_iter()
+                     .map(|x| x.split_whitespace().collect::<Vec<_>>()[0].parse::<i32>().unwrap())
+                     .collect();
+
+    let mut list2 : Vec<_> = input.into_iter()
+                    .map(|x| x.split_whitespace().collect::<Vec<_>>()[1].parse::<i32>().unwrap())
+                    .collect();
+
+    list1.sort();
+    list2.sort();
+
+    // PART 1
+    let result : i32 = list1.iter().zip(list2.iter())
+                    .map(|(x, y)| (x - y).abs())
+                    .sum();
+
+    println!("{}", result);
+
+    // PART 2
+    let result2 : i32 = list1.iter()
+                    .map(|x| x * (list2.clone().into_iter().filter(|y| y == x).count() as i32))
+                    .sum();
+    println!("{}", result2);
 }
