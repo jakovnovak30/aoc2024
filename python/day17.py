72142243d214d4baf06cc2b26d69c15ed94d87e6
jakovnovak30 <jakovnovak30@gmail.com>
2024-12-17 14:39:57 +0100

    day17 done

--- python/day17.py
+++ python/day17.py
@@ -1,5 +1,7 @@
 #!/bin/python3
 
+import z3
+
 def get_combo_operand(combo_code, registers):
     if combo_code >= 0 and combo_code <= 3:
         return combo_code
@@ -16,7 +18,6 @@ def part1(registers, instructions) -> str:
     while pc < len(instructions):
         opcode = instructions[pc]
 
-        #print(instructions[pc])
         match opcode:
             case 0:
                 operand1 = registers[0]
@@ -61,7 +62,89 @@ def part1(registers, instructions) -> str:
                 pc += 2
 
     return ','.join(map(str, output))
-                
+
+def part2(instructions) -> int:
+    instructions = instructions
+    opcodes = instructions[:-2:2]
+    operands = instructions[1:-2:2]
+
+    s = z3.Solver()
+    iteration_count = len(instructions)
+    A = [[z3.BitVec(f'A_{i}_{j}', 128) for j in range(len(opcodes)+1)] for i in range(iteration_count)]
+    B = [[z3.BitVec(f'B_{i}_{j}', 128) for j in range(len(opcodes)+1)] for i in range(iteration_count)]
+    C = [[z3.BitVec(f'C_{i}_{j}', 128) for j in range(len(opcodes)+1)] for i in range(iteration_count)]
+
+    def get_combo_ref(combo_code, a, b, c, index):
+        if combo_code >= 0 and combo_code <= 3:
+            return z3.BitVecVal(combo_code, 128)
+
+        if combo_code == 4:
+            return a[index]
+        elif combo_code == 5:
+            return b[index]
+        elif combo_code == 6:
+            return c[index]
+
+        raise RuntimeError
+
+    for iteration, (a, b, c) in enumerate(zip(A, B, C)):
+        if iteration == 0:
+            s.add(b[0] == 0)
+            s.add(c[0] == 0)
+        else:
+            s.add(a[0] == A[iteration-1][len(opcodes)])
+            s.add(b[0] == B[iteration-1][len(opcodes)])
+            s.add(c[0] == C[iteration-1][len(opcodes)])
+
+        for index, (opcode, operand) in enumerate(zip(opcodes, operands)):
+            match opcode:
+                case 0:
+                    operand2 = get_combo_ref(operand, a, b, c, index)
+
+                    s.add(a[index+1] == a[index] >> operand2)
+                    s.add(b[index+1] == b[index])
+                    s.add(c[index+1] == c[index])
+                case 1:
+                    s.add(a[index+1] == a[index])
+                    s.add(b[index+1] == b[index] ^ z3.BitVecVal(operand, 128))
+                    s.add(c[index+1] == c[index])
+                case 2:
+                    s.add(a[index+1] == a[index])
+                    s.add(b[index+1] == get_combo_ref(operand, a, b, c, index) % 8)
+                    s.add(c[index+1] == c[index])
+                case 4:
+                    s.add(a[index+1] == a[index])
+                    s.add(b[index+1] == b[index] ^ c[index])
+                    s.add(c[index+1] == c[index])
+                case 5:
+                    s.add(a[index+1] == a[index])
+                    s.add(b[index+1] == b[index])
+                    s.add(c[index+1] == c[index])
+
+                    combo = get_combo_ref(operand, a, b, c, index)
+                    s.add(z3.BitVecVal(instructions[iteration], 128) == combo % 8)
+                case 6:
+                    operand2 = get_combo_ref(operand, a, b, c, index)
+
+                    s.add(a[index+1] == a[index])
+                    s.add(b[index+1] == a[index] >> operand2)
+                    s.add(c[index+1] == c[index])
+                case 7:
+                    operand2 = get_combo_ref(operand, a, b, c, index)
+
+                    s.add(a[index+1] == a[index])
+                    s.add(b[index+1] == b[index])
+                    s.add(c[index+1] == a[index] >> operand2)
+
+        # conditions for a: needs to be different than zero for every iteration except the last one:
+        if iteration == iteration_count - 1:
+            s.add(a[len(opcodes)] == 0)
+        else:
+            s.add(a[len(opcodes)] != 0)
+
+    # check model
+    s.check()
+    return s.model()[A[0][0]]
 
 if __name__ == '__main__':
     registers = []
@@ -77,3 +160,4 @@ if __name__ == '__main__':
         instructions = list(map(int, last_line.split(' ')[-1].split(',')))
 
     print(f"Solution 1: {part1(registers, instructions)}")
+    print(f"Solution 2: {part2(instructions)}")
