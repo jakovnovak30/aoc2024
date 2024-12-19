0d3ef7797c782a4ffcd2b64aea5c4c6c3f03b167
jakovnovak30 <jakovnovak30@gmail.com>
2024-12-19 12:28:17 +0100

    day19 done

--- python/day19.py
+++ python/day19.py
@@ -1,5 +1,7 @@
 #!/bin/python3
 
+from functools import cache
+
 def part1(availible : list[str], desired : list[str]) -> int:
     def check_possible(curr : str, goal : str) -> bool:
         if curr == goal:
@@ -13,11 +15,31 @@ def part1(availible : list[str], desired : list[str]) -> int:
             or_part = or_part or check_possible(curr + next, goal)
 
         return or_part
-        
 
     ctr = 0
     for goal in desired:
         ctr += check_possible('', goal)
+
+    return ctr
+
+def part2(availible : list[str], desired : list[str]) -> int:
+    @cache
+    def check_options(curr : str, goal : str) -> int:
+        if curr == goal:
+            return 1
+        elif len(curr) >= len(goal) or goal[:len(curr)] != curr:
+            return 0
+
+        or_part = 0
+        # check other options
+        for next in availible:
+            or_part += check_options(curr + next, goal)
+
+        return or_part
+
+    ctr = 0
+    for goal in desired:
+        ctr += check_options('', goal)
 
     return ctr
 
@@ -33,3 +55,4 @@ if __name__ == '__main__':
 
 
     print(f"Solution 1: {part1(availible, desired)}")
+    print(f"Solution 2: {part2(availible, desired)}")
